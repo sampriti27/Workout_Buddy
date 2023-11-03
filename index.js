6bd2ea8a81d92103c9e4c4ddd4d3abe968d43dc6
Sampriti Mukherjee <sampriti275@gmail.com>
2023-11-03 21:32:57 +0530

    production grade

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/users");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -20,14 +21,26 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING!");
+  });
+}
+
 //connect to db
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log("Connected to db and listening on port", process.env.PORT);
-    });
+    console.log("Connected to db and listening on port");
   })
   .catch((error) => {
     console.log(error);
